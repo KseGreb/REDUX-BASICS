1122359ca0d93300fe1e1a032359628afe02e4db
“KseGreb” <ksegreb@gmail.com>
2024-02-22 11:02:21 -0700

    logic and steps in index.js

--- src/index.js
+++ src/index.js
@@ -6,22 +6,59 @@ import reportWebVitals from './reportWebVitals';
 
 
 
+
 //1 - STORE - ALL DATA
 
+let store = createStore(cart)
+
 //2 - ACTION
+      // All logic starts with action that user/customer could possibly do OR WANT TO DO  on our website (FOR EXAMPLE WE HAVE ONLINE STORE)
+      //We don't change state here, this is only possibility of what happened, but no guarantee that customer will actually do it (add to cart or buy something)
 
-// All logic starts with action
-
+      //A
 const addToCart = () => {
   return {
     type: "ADD_TO_CART"
   }
 }
-
+      //B
+const removeItem = () => {
+  return {
+    type: "REMOVE_ITEM"
+  }
+}
+      //C
+const changeNumber = () => {
+  return {
+    type: "CHANGE_NUMBER"
+  }
+}
 
 //3 - REDUCER
+      //DESCRIPTION of how our possible actions will change a state. But we still don't chcange stte here,
+      //only explaining how it will be changed in the future based on the action that happened
+
+const cart = (state = 0, action) => {
+  switch(action.type) {
+    case "ADD_TO_CART":
+      return state + 1;
+
+    case "REMOVE_ITEM":
+      return state - 1;
+
+    default:
+      return state;
+  }
+}
+
 
 //4 - DISPATCH
+      //this is the only way to change the state! we need to call store.dispatch() and pass in an action object 
+
+store.dispatch(addToCart())
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
